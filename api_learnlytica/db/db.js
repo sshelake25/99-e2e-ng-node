bd6dc22e5b93927313ab82dea8b37dfe0b5d3a20
Surekha Shelake <shelakesurekha@gmail.com>
2022-05-12 10:26:54 +0530

    create sample data for our testing

--- api_learnlytica/db/db.js
+++ api_learnlytica/db/db.js
@@ -5,11 +5,6 @@ require('dotenv').config();
 
 let mysqlConnect = function () {
     return mysql.createConnection({
-        // host: process.env.DB_HOST,
-        // user: process.env.DB_USER,
-        // password: process.env.DB_PASSWORD,
-        // database: process.env.DB_NAME,
-        // insecureAuth : true
         host: process.env.DB_HOST,
         user: process.env.DB_USER,
         password: process.env.DB_PASSWORD, //should be yours 
@@ -19,18 +14,31 @@ let mysqlConnect = function () {
     });
 };
 
-let createTable = `create table if not exists user_profile(
-    id int primary key auto_increment,
-    title varchar(255)not null,
-    completed tinyint(1) not null default 0
-)`;
 
-mysqlConnect().query(createTable, (error, results, fields) => {
-    if (error) throw error;
-});
+function createBaseTables() {
+    let createTable = `create table if not exists ss_profile(
+        id int primary key auto_increment,
+        title varchar(255)not null,
+        completed tinyint(1) not null default 0
+    )`;
 
-// mysqlConnect().query(insetData, (error, results, fields) => {
-//     if (error) throw error;
-// });
+    mysqlConnect().query(createTable, (error, results, fields) => {
+        if (error) throw error;
+    });
+}
+
+function insetTableData() {
+    let insetData = `INSERT INTO ss_profile (title, completed) VALUES ('ccc', '1'), ('dd', '2')`;
+
+    mysqlConnect().query(insetData, (error, results, fields) => {
+        if (error) throw error;
+    });
+}
+
+
+createBaseTables();
+
+insetTableData();
+
 
 module.exports.localConnect = mysqlConnect;
